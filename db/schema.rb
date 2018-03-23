15c384870317901af864b9d3988ce3f188ac4c90
[jawdinmorris] <[jordanandrewmorris1@gmail.com]>
2018-03-23 14:07:06 +1000

    failing to add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322124220) do
+ActiveRecord::Schema.define(version: 20180323030836) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20180322124220) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
